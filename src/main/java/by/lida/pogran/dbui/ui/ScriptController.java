0f02ada2412b7e00b76313ff95af9d9aa6cb0c1b
Administrator <ast4pastap@yandex.ru>
2024-03-08 11:53:29 +0300

    try2

--- src/main/java/by/lida/pogran/dbui/ui/ScriptController.java
+++ src/main/java/by/lida/pogran/dbui/ui/ScriptController.java
@@ -1,18 +1,15 @@
 package by.lida.pogran.dbui.ui;
 
-import by.lida.pogran.dbui.config.CustomDataSourceConfiguration;
 import by.lida.pogran.dbui.config.OracleConfigurationProperties;
 import by.lida.pogran.dbui.entity.SQLScript;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -31,7 +28,6 @@ public class ScriptController {
 
     @FXML
     public void startScript() {
-        Connection connection = null;
         String value = scripts.getValue();
         SQLScript selected = Arrays.stream(SQLScript.values()).filter(a -> a.getName().equals(value)).findFirst().get();
 //        String scriptPath = Arrays.stream(SQLScript.values()).filter(a -> a.getName().equals(value)).map(SQLScript::getPath).findFirst().get();
@@ -49,42 +45,19 @@ public class ScriptController {
 //            throw new RuntimeException(e);
 //        }
 
-        try {
-            connection = OracleConfigurationProperties.getConnection();
+        try (Connection connection = OracleConfigurationProperties.getConnection()) {
             connection.setAutoCommit(false);
-            PreparedStatement callableStatement = connection.prepareStatement(selected.getScript());
-            callableStatement.executeBatch();
-            connection.commit();
-            boolean execute = callableStatement.execute();
-            if (execute) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Query Status:");
-                alert.setContentText(selected.getName() + " успешно выполнен\n");
-                alert.showAndWait();
-            }
-            ResultSet res = callableStatement.getResultSet();
-            while (res.next()) {
 
+            try (PreparedStatement mergeStatements = connection.prepareStatement(selected.getScript())) {
+                mergeStatements.executeUpdate();
             }
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Query Status:");
+            alert.setContentText(selected.getName() + " успешно выполнен\n");
+            alert.showAndWait();
+            connection.commit();
         } catch (SQLException e) {
-            // Handle exceptions, log errors, and rollback the transaction on failure
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (SQLException rollbackException) {
-                    rollbackException.printStackTrace();
-                }
-            }
             e.printStackTrace();
-        } finally {
-            // Close the connection in a finally block
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException closeException) {
-                    closeException.printStackTrace();
-                }
-            }
         }
     }
 }
