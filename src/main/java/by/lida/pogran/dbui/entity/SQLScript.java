f3e58714a2192a7abc7e613766ba3a2121a346b6
Administrator <ast4pastap@yandex.ru>
2024-03-18 17:25:46 +0300

    upd

--- src/main/java/by/lida/pogran/dbui/entity/SQLScript.java
+++ src/main/java/by/lida/pogran/dbui/entity/SQLScript.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 
 @Getter
 public enum SQLScript {
-    SCRIPT_1("users_asoosd", SQLConstants.users_asoosd,"src/main/resources/scripts/users_ASOOSD.sql",""),
-    SCRIPT_2("z1z4", SQLConstants.users_asoosd,"src/main/resources/scripts/#z1-z4.sql",""),
-    SCRIPT_3("select1",SQLConstants.users_asoosd,"",""),
-    SCRIPT_4("select1", SQLConstants.users_asoosd,"",""),
-    SCRIPT_5("select2", SQLConstants.users_asoosd,"",""),
-    SCRIPT_6("select3", SQLConstants.users_asoosd,"",""),
-    SCRIPT_7("select4", SQLConstants.users_asoosd,"",""),
+    SCRIPT_1("users_asoosd", SQLConstants.users_asoosd,"sss.sql","скрипт для ....users_asoosd!."),
+    SCRIPT_2("z1z4", SQLConstants.users_asoosd,"src/main/resources/scripts/#z1-z4.sql","скрипт для ....ОПИСАНИЕ2!."),
+    SCRIPT_3("select1",SQLConstants.users_asoosd,"","скрипт для ....ОПИСАНИЕ3!."),
+    SCRIPT_4("select1", SQLConstants.users_asoosd,"","скрипт для ....ОПИСАНИЕ4!."),
+    SCRIPT_5("select2", SQLConstants.users_asoosd,"","скрипт для ....ОПИСАНИЕ5!."),
+    SCRIPT_6("select3", SQLConstants.users_asoosd,"","скрипт для ....ОПИСАНИЕ6!."),
+    SCRIPT_7("select4", SQLConstants.users_asoosd,"","скрипт для ....ОПИСАНИЕ7!."),
     SCRIPT_8("select5", SQLConstants.users_asoosd,"",""),
     SCRIPT_9("select6", SQLConstants.users_asoosd,"",""),
     SCRIPT_10("select7", SQLConstants.users_asoosd,"",""),
